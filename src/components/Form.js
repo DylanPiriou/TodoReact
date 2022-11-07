ba61d08734e9847e5648695d8e7af9122bbe7290
DylanPiriou <dylan.piriou@hotmail.com>
2022-11-07 16:48:19 +0100

    responsive, localstorage and better ui

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,82 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Todos from "./Todos";
 import "./Form.css";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 export default function Form() {
+  // state des todos
   const [dataArr, setDataArr] = useState([]);
+  // state de l'input
+  const [stateInput, setStateInput] = useState("");
 
-  const [stateInput, setStateInput] = useState();
+  // fonction pour ajouter des todos
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (stateInput === "") {
+      return;
+    }
 
-  const addTodo = e => {
-    e.preventDefault()
+    const newArr = [...dataArr];
+    const newTodo = {};
+    newTodo.txt = stateInput;
+    newTodo.id = uuidv4();
 
-    const newArr = [...dataArr]
+    newArr.push(newTodo);
+    setDataArr(newArr);
+    setStateInput("");
+    localStorage.setItem("todos", JSON.stringify(newArr));
+  };
 
-    const newTodo = {}
-      newTodo.txt = stateInput
-      newTodo.id = uuidv4()
+  // fonction pour donner la valeur de l'input au state
+  const linkedInput = (e) => {
+    setStateInput(e);
+  };
 
-    newArr.push(newTodo)
-    setDataArr(newArr)
-    setStateInput("")
-  }
-  
-  const linkedInput = e => {
-    setStateInput(e)
-  }
-  
+  // fonction pour supprimer des todos
   const handleDel = (id) => {
-    const filteredState = dataArr.filter(item => {
-      return item.id !== id
-    })
+    const filteredState = dataArr.filter((item) => item.id !== id);
+    window.localStorage.todos = JSON.stringify(filteredState);
     setDataArr(filteredState)
-  }
-  
+  };
+
+  useEffect(() => {
+    let todos = [];
+    const todosJson = localStorage.getItem("todos");
+    if (todosJson) {
+      todos = JSON.parse(todosJson);
+      setDataArr(todos);
+    }
+  }, []);
+
   return (
     <div className="todo-container">
-      <form onSubmit={e => addTodo(e)} >
+      <form onSubmit={(e) => addTodo(e)}>
         <input
-        value={stateInput}
-        onChange={e => linkedInput(e.target.value)}
+          value={stateInput}
+          onChange={(e) => linkedInput(e.target.value)}
           type="text"
           placeholder="Entrer un todo"
-          />
+        />
         <button>Ajouter</button>
       </form>
-      <ul className="list-todos" >
-        {dataArr.map(todo => {
-        return <Todos key={todo.id} id={todo.id} todo={todo.txt} delFunc={handleDel} />;
-      })}
-      </ul>
+      {/* {stateInput === "" ? <p>Entrer des todos</p> : null} */}
+      <div className="list-todos">
+        {dataArr.length > 0 ? (
+          dataArr.map((todo) => {
+            return (
+              <Todos
+                input={stateInput}
+                key={todo.id}
+                id={todo.id}
+                todo={todo.txt}
+                delFunc={handleDel}
+              />
+            );
+          })
+        ) : (
+          <p className="default-txt">Pas de todos.😥</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,9 +4,12 @@ import "./Todos.css"
 export default function Todos(props) {
   
     return (
+      <>
+      {props.todo &&
     <div className="todo">
-        <h3>{props.todo}</h3>
-        <div onClick={() => props.delFunc(props.id)} className="btn-delete">x</div>
-    </div>
+      <h3>{props.todo}</h3>
+        <div onClick={() => props.delFunc(props.id)} className="btn-delete"><i className="fa-solid fa-trash"></i></div>
+    </div>}
+    </>
   )
 }
