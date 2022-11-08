984af230015b852dbd404f988d38f9769ec75c7a
DylanPiriou <dylan.piriou@hotmail.com>
2022-11-08 08:55:56 +0100

    last v

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Todos.css"
 
 export default function Todos(props) {
+  const [todo, setTodo] = useState(false)
+    
+  const handleTodo = () => {
+      setTodo(!todo)
+    }
   
-    return (
+  return (
       <>
       {props.todo &&
-    <div className="todo">
+    <div onClick={() => handleTodo()} className="todo" style={{textDecoration: todo ? "line-through" : "none"}} >
       <h3>{props.todo}</h3>
         <div onClick={() => props.delFunc(props.id)} className="btn-delete"><i className="fa-solid fa-trash"></i></div>
     </div>}
